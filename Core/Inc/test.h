f66bef4b6e31405715288cd9cc89b905a7d93460
codedraughtsman <codedraughtsman@users.noreply.github.com>
2020-10-30 12:07:29 +1300

    Converted test.h to use logger

--- Core/Inc/test.h
+++ Core/Inc/test.h
@@ -8,10 +8,12 @@
 #ifndef __TEST_H__
 #define __TEST_H__
 
+#include "logger.h"
 
 #define ASSERT_EQ_INT(var1, var2, message) if (var1 != var2) {\
-	printf("assert failed: " #var1 " is not equal to " #var2 "\n\t values are %d and %d\n",var1, var2);\
-	printf("\t%s\n",message);\
+	logger_print("assert failed: " #var1 " is not equal to " #var2 "\n" );\
+	logger_print("\t values are %d and %d\n",var1, var2);\
+	logger_print("\t%s\n",message);\
 	return -1;\
 }
 
